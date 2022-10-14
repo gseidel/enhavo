bf63d98ccdf10431efb0fbbb629466a9c11e5b82
Gerhard Seidel <gseidel.message@googlemail.com>
2022-10-14 15:50:17 +0200

    Vue forms (#1653)
    | Q             | A
    | ------------- | ---
    | Bug fix?      | no
    | New feature?  | yes
    | Doc PR?       | no
    | License       | MIT
    
    * Add `vue-components` and `vue-factory` compiler
    * Add `ListVueType` and `PositionVueType`
    * Allow multiple rendering
    * Use `supports` funtion for VueType

--- Form/VueType/RadioVueType.php
+++ Form/VueType/RadioVueType.php
@@ -2,26 +2,20 @@
 
 namespace Enhavo\Bundle\VueFormBundle\Form\VueType;
 
+use Enhavo\Bundle\VueFormBundle\Form\AbstractVueType;
 use Enhavo\Bundle\VueFormBundle\Form\VueData;
-use Enhavo\Bundle\VueFormBundle\Form\VueTypeInterface;
-use Symfony\Component\Form\Extension\Core\Type\RadioType;
-use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 
-class RadioVueType implements VueTypeInterface
+class RadioVueType extends AbstractVueType
 {
-    public function getComponent(): ?string
+    public static function supports(FormView $formView): bool
     {
-        return 'form-radio';
-    }
-
-    public static function getBlocks(): array
-    {
-        return ['radio' => 1];
+        return in_array('radio', $formView->vars['block_prefixes']);
     }
 
     public function buildView(FormView $view, VueData $data)
     {
         $data['fullName'] = $view->vars['full_name'];
+        $data['component'] = 'form-radio';
     }
 }
